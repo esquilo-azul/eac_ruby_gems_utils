8681dc4110cbe4a6adf1786e49e5440e5e85da50
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-29 17:37:43 -0300

    EacRubyGemsUtils::Gem::VersionFile.

---
+++ lib/eac_ruby_gems_utils/gem/version_file.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacRubyGemsUtils
+  class Gem
+    class VersionFile
+      common_constructor :path
+
+      VERSION_LINE_PATTERN = /\A(\s*)VERSION\s*=\s*[\'\"]([^\'\"]+)[\'\"](\s*)\z/.freeze
+
+      def value
+        path.read.each_line do |line|
+          VERSION_LINE_PATTERN.if_match(line.rstrip, false) do |m|
+            ::Gem::Version.new(m[2])
+          end
+        end
+      end
+
+      def value=(new_value)
+        path.write(new_value_content(new_value))
+      end
+
+      private
+
+      def new_value_content(new_value)
+        path.read.each_line
+            .map { |line| new_value_line(line, new_value) }
+            .join
+      end
+
+      def new_value_line(line, new_value)
+        m = VERSION_LINE_PATTERN.match(line)
+        return line unless m
+
+        "#{m[1]}VERSION = '#{new_value}'#{m[3]}"
+      end
+    end
+  end
+end
